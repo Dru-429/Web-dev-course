fb971ca04d7e80c1fc5a0c18ffa2be336c558033
Dru-429 <ect.dhruv@gmail.com>
2025-02-09 17:58:43 +0530

    Initiating Server

--- JSM/subscription-tracker/app.js
+++ JSM/subscription-tracker/app.js
@@ -1,1 +1,13 @@
-console.log('server runing localhost:3000')
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Subcription Tracker API!')
+})
+
+app.listen(3000, () => {
+  console.log('Server runnig on http://localhost:3000')
+})
+
+export default app 
