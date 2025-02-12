bb0dfc7b4ea446e4108a7e4e0988838df4ba3a5f
Dru-429 <ect.dhruv@gmail.com>
2025-02-12 18:29:05 +0530

    Adding other imp Middelwares

--- JSM/subscription-tracker/app.js
+++ JSM/subscription-tracker/app.js
@@ -10,9 +10,9 @@ import errorMiddelware from "./middlewares/error.middleware.js"
 
 const app = express()
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false}))
-// app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
