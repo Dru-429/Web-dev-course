def1184e187fd4aa9beecd84a8285176eb172baa
Dru-429 <ect.dhruv@gmail.com>
2025-02-13 07:57:14 +0530

    SIGN UP controlled added

--- JSM/subscription-tracker/controllers/auth.controller.js
+++ JSM/subscription-tracker/controllers/auth.controller.js
@@ -1,5 +1,56 @@
-export const signUp = async (req, res, next) => {}
+import mongoose from "mongoose";
+import User from "../models/users.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const signIn = async (req, res, next) => {}
+//What is req body --- a object that contains the data from client side , eg if we do post to create a user it send a req that contains usernsme, email ,passwd etc
 
-export const signOut = async (req, res, next) => {}
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { name, email, password } = req.body;
+
+    //checking if the user already exits
+    const existingUser = await User.findOne();
+    if (existingUser) {
+      const error = new Error("User already exits..");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    //Else hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {};
+
+export const signOut = async (req, res, next) => {};
