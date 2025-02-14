a9ca024cc68ff9ff195afe8f69b45abd3cfcfd15
Dru-429 <ect.dhruv@gmail.com>
2025-02-14 10:58:04 +0530

    Adding Subscription controller

--- JSM/subscription-tracker/controllers/subscription.controller.js
+++ JSM/subscription-tracker/controllers/subscription.controller.js
@@ -4,7 +4,7 @@ export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({
       ...req.body,
-      user: req.user._id,
+      user: req.user._id,  
     });
 
     res.status(201).json({
