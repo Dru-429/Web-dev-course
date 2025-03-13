7c7d60b75235e63f212bfe0e7906a70192e14117
Dru-429 <ect.dhruv@gmail.com>
2025-03-13 21:16:16 +0530

    TS video-4

---
+++ TypeScript/video3/index.ts
@@ -0,0 +1,9 @@
+//how to type a veriable in ts
+
+// let myName = "Dhruv"  JS
+
+let myName: string = "Bob"  //TS
+// Primitive Data Types: string, number, boolean
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
