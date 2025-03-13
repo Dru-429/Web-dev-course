8c4929786b4d4c29c6392cf9c5ae69b980618e98
Dru-429 <ect.dhruv@gmail.com>
2025-03-13 23:42:23 +0530

    TS video-7

--- TypeScript/video3/index.ts
+++ TypeScript/video3/index.ts
@@ -9,3 +9,28 @@ let numberOfWheels: number = 4
 let isStudent: boolean = false
 
 
+// video 6 : custom types
+
+type Food = string
+
+let favFood : Food = "Pizza"
+
+
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    // isstudent: false
+    isStudent: false
+}
