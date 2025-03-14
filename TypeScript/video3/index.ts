aa4c47125ab5f3500fbcdc9cbdb25ee18f25b44c
Dru-429 <ect.dhruv@gmail.com>
2025-03-14 23:42:52 +0530

    Video 25

--- TypeScript/video3/index.ts
+++ TypeScript/video3/index.ts
@@ -130,6 +130,7 @@ const myName3 = "Bobby" //const myName3: "Bobby"
 // union can be only fixed types of string 
 
 type User = {
+    id: number
     username: string
     role: "guest" | "member" | "admin"
 }
@@ -148,9 +149,9 @@ let userRole: UserRole = "admin"
 //video 21 : Definig function return type and also beign explicite
 
 const users: User[] = [
-    { username: "john_doe", role: "member" },
-    { username: "jane_doe", role: "admin" },
-    { username: "guest_user", role: "guest" }
+    {id:1, username: "john_doe", role: "member" },
+    { id: 2, username: "jane_doe", role: "admin" },
+    { id: 3, username: "guest_user", role: "guest" }
 ];
 
 // function fetchUserDetails(username: string)  {  //function fetchUserDetails(username: string): User
@@ -181,3 +182,39 @@ console.log(a.toUpperCase)
 
 // In short: DON'T 😉
 // One legitimate use-case: you're in the process of transitioning a code base from JavaScript to TypeScript and don't immediately have the time to write all your complex types, so you just need a way to "get around TypeScript" for now.
+
+//video28 : Utility Type & Partial
+
+// type UpdatedUser = {
+//     id?: number
+//     username?: string
+//     role?: "member" | "contributor" | "admin"
+// }
+
+//or 
+
+type UpdatedUser = Partial<User>
+
+
+function updateUser( id: number, updates: UpdatedUser) {
+    const currUser = users.find( user => user.id === id )
+    if( !currUser){
+        console.log("undefine ")
+        return
+    }
+    Object.assign(currUser, updates)
+    return 
+}
+
+updateUser(1, { username: "new_john_doe" });
+updateUser(4, { role: "contributor" });
+
+// Utility Types
+
+// 01 Like a function, they take other types in as a parameter and return a new type, with some changes made to it.
+// 02 Built-in to TypeScript; perform commonly-needed modifications to existing types
+// 03 Use "Generics" syntax using angle brackets (<>)
+
+//Partial Type 
+
+//It modifies all the types you give it to , to the optional type 
