401e4161e56fd9997642c0bb26d824276a8962c6
Dru-429 <ect.dhruv@gmail.com>
2025-03-15 15:18:03 +0530

    Without ShadCN

---
+++ ShadCN/crash-course/src/app/chai/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ChaiPage = () => {
+  return (
+    <main className='h-screen flex justify-center items-center gap-2 flex-col'>
+
+        <div>
+            ChaiPage 🍵
+        </div>
+
+        <button className='py-2 px-4 bg-blue-400 hover:bg-blue-700 rounded-2xl'>
+            Test button 
+        </button>
+    </main>
+  )
+}
+
+export default ChaiPage
